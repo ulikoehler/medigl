af9c011ec23ae90611a1bf26df368d5f7edfd041
Uli Köhler <ulrich.koehler@helmholtz-muenchen.de>
2010-06-13 17:02:33 +0200

    There are VHP TARs available; changed the DICOM download script to use them

--- devscripts/download-vhp-dicom.sh
+++ devscripts/download-vhp-dicom.sh
@@ -1,58 +1,14 @@
 #!/bin/sh
 #This quick-and-dirty scripts downloads the DICOM files from the Visible Human Project
-echo "Downloading Ankle DICOM data"
-mkdir Ankle
-wget https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Ankle/thumbnails/Ankle.html
-egrep "\.\./.*\.dcm" Ankle.html -o | cut -c 4- > Ankle.txt
-for line in `cat Ankle.txt`
-	do
-		echo $line
-		echo "https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Ankle/"$line | xargs wget -q -P Ankle
-	done
-rm Ankle.html Ankle.txt
-echo "Downloading Head DICOM data"
-mkdir Head
-wget https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Head/thumbnails/Head.html -q
-egrep "\.\./.*\.dcm" Head.html -o | cut -c 4- > Head.txt
-for line in `cat Head.txt`
-	do
-		echo $line
-		echo "https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Head/"$line | xargs wget -q -P Head
-	done
-rm Head.html Head.txt
-mkdir Hip
-wget https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Hip/thumbnails/Hip.html -q
-egrep "\.\./.*\.dcm" Hip.html -o | cut -c 4- > Hip.txt
-for line in `cat Hip.txt`
-	do
-		echo $line
-		echo "https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Hip/"$line | xargs wget -q -P Hip
-	done
-rm Hip.html Hip.txt
-mkdir Knee
-wget https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Knee/thumbnails/Knee.html -q
-egrep "\.\./.*\.dcm" Knee.html -o | cut -c 4- > Knee.txt
-for line in `cat Knee.txt`
-	do
-		echo $line
-		echo "https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Knee/"$line | xargs wget -q -P Knee
-	done
-rm Knee.html Knee.txt
-mkdir Pelvis
-wget https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Pelvis/thumbnails/Pelvis.html -q
-egrep "\.\./.*\.dcm" Pelvis.html -o | cut -c 4- > Pelvis.txt
-for line in `cat Pelvis.txt`
-	do
-		echo $line
-		echo "https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Pelvis/"$line | xargs wget -q -P Pelvis
-	done
-rm Pelvis.html Pelvis.txt
-mkdir Shoulder
-wget https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Shoulder/thumbnails/Shoulder.html -q
-egrep "\.\./.*\.dcm" Shoulder.html -o | cut -c 4- > Shoulder.txt
-for line in `cat Shoulder`
-	do
-		echo $line
-		echo "https://mri.radiology.uiowa.edu//VHDicom/VHFCT1mm/Shoulder/"$line | xargs wget -q -P Pelvis
-	done
-rm Shoulder.html Shoulder.txt
+mkdir Male
+mkdir Female
+wget -P Female http://mri.radiology.uiowa.edu/VHDicom/VHFCT1mm/VHF-Ankle.tar.gz
+wget -P Female http://mri.radiology.uiowa.edu/VHDicom/VHFCT1mm/VHF-Head.tar.gz
+wget -P Female http://mri.radiology.uiowa.edu/VHDicom/VHFCT1mm/VHF-Hip.tar.gz
+wget -P Female http://mri.radiology.uiowa.edu/VHDicom/VHFCT1mm/VHF-Knee.tar.gz
+wget -P Female http://mri.radiology.uiowa.edu/VHDicom/VHFCT1mm/VHF-Pelvis.tar.gz
+wget -P Female http://mri.radiology.uiowa.edu/VHDicom/VHFCT1mm/VHF-Shoulder.tar.gz
+wget -P Male http://mri.radiology.uiowa.edu/VHDicom/VHMCT1mm/VHMCT1mm_Head.tar.gz
+wget -P Male http://mri.radiology.uiowa.edu/VHDicom/VHMCT1mm/VHMCT1mm_Shoulder.tar.gz
+wget -P Male http://mri.radiology.uiowa.edu/VHDicom/VHMCT1mm/VHMCT1mm_Pelvis.tar.gz
+wget -P Male http://mri.radiology.uiowa.edu/VHDicom/VHMCT1mm/VHMCT1mm_Hip.tar.gz
