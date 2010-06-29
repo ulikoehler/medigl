2bb56433cf8c80db26251c13c7af5b394df9bfdb
Uli Köhler <ulrich.koehler@helmholtz-muenchen.de>
2010-06-29 22:09:25 +0200

    Added some code to FastImage

--- medigl/fastimage.cpp
+++ medigl/fastimage.cpp
@@ -1,5 +1,49 @@
 #include "fastimage.h"
 
-FastImage::FastImage()
+//Macro for adressing pixels int virtually 2d arrays
+#define REL_ADDR_2D(width, x, y) (x * width + y)
+
+FastImage::FastImage(QImage* img, bool enableGrayCache)
 {
+    this->grayCacheEnabled = enableGrayCache;
+    int width = img->width();
+    int height = img->height();
+    this->width = img->width();
+    this->height = img->height();
+    this->colorData = new int[width * height];
+    for(int x = 0; x < width; x++)
+    {
+        for(int y = 0; y < width; y++)
+        {
+            colorData[REL_ADDR_2D(width, x, y)] = img->pixel(x, y);
+        }
+    }
+    //Build the gray cache if enabled
+    if(enableGrayCache)
+    {
+        grayData = new char[width * height];
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < width; y++)
+            {
+                grayData[REL_ADDR_2D(width, x, y)] = qGray(img->pixel(x, y));
+            }
+        }
+    }
+    else
+    {
+        grayData = 0;
+    }
+}
+
+FastImage::FastImage(uint width, uint height)
+{
+    this->grayCacheEnabled = enableGrayCache;
+    this->width = width;
+    this->height = height;
+}
+
+FastImage::~FastImage()
+{
+
 }
--- medigl/fastimage.h
+++ medigl/fastimage.h
@@ -2,11 +2,53 @@
 #define FASTIMAGE_H
 
 #include <QImage>
+#include <stdint.h>
 
+/**
+ * Image wrapper class internally using arrays for fast read access
+ * Optimized for read access
+ * Compiling with -fno-strict-aliasing should make this class faster
+ *
+ * One instance of this class represents exactly one image.
+ * FastImage can build a gray cache to be able to serve getGray() requests
+ * very fast. This feature is enabled by default but can be disabled.
+ * Users are not encouraged to change the data using setPixel(...).
+ *
+ * FastImage only supports RGBA with 8 bits per color.
+ *
+ * The purpose of this class is to have a fast, scalable abstraction layer between
+ * MediGL input data and OpenGL in order to be able to support a variety of image formats.
+ */
 class FastImage
 {
 public:
-    FastImage();
+    /**
+     * Creates a new FastImage instance from a QImage.
+     * Uses the data from the QImage instance to fill the cache
+     * \param img The image to process
+     * \param enableGrayCache Whether to enable a separate gray cache
+     */
+    FastImage(QImage* img, bool enableGrayCache = true);
+    /**
+     * Creates a new FastImage instance with given width and height
+     * but without any content.
+     * Use setPixel(...) to set the pixels
+     * \param width The widi of the new image
+     * \param height The height of the new image
+     * \param enableGrayCache Whether to enable a separate gray cache
+     */
+    FastImage(uint width, uint height, bool enableGrayCache = true);
+    ~FastImage();
+    uint32_t getRgb(uint x, uint y);
+    char getGray(uint x, uint y);
+    void setPixel(uint x, uint y, int val);
+    uint getWidth();
+    uint getHeight();
+protected:
+    uint width, height
+    bool grayCacheEnabled;
+    uint32_t* colorData;
+    char* grayData;
 };
 
 #endif // FASTIMAGE_H
